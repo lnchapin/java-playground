44aef4b32f00a91ebd7b805665f0d904c8a5cb98
Lindsay Chapin <lnchapin@gmail.com>
2018-08-16 00:17:50 -0600

    wip 2.2

--- src/main/java/com/galvanize/BankAccount.java
+++ src/main/java/com/galvanize/BankAccount.java
@@ -2,5 +2,25 @@ package com.galvanize;
 
 public class BankAccount {
     private String name;
-    private String balance;
+    private float balance;
+    private boolean isOverDrawn;
+
+    public void deposit (float value){
+        balance += value;
+    }
+    public void withdraw (float value){
+        balance -= value;
+    }
+
+    public void balance() {
+        System.out.printf("balance");
+    }
+
+    public void isOverDrawn(){
+        if (balance < 0) {
+            isOverDrawn = true;
+        } else {
+            isOverDrawn = false;
+        }
+    }
 }
